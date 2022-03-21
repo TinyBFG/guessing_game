0d7f9e7bb46f1911337d1692e2fd5b6b65162741
TinyBFG <tinybfg337@gmail.com>
2022-03-21 15:55:28 -0500

    Finished guessing game and working on the tests

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -2,18 +2,61 @@ defmodule GuessingGame do
   @moduledoc """
   Documentation for `GuessingGame`.
   """
+
   # call level 1
 
+  def intro() do
+    name = IO.gets("Hello, what's your name?\n")
+    name = String.trim(name)
+    name_response(name)
+  end
+
+  # call level 2
+
+  def name_response("Ian") do
+    yes_or_no =
+      IO.gets("That's awesome!  My creator is an Ian!  Would you like to play a game?\n")
+
+    yes_or_no = String.trim(yes_or_no)
+    maybe_play_game(yes_or_no)
+  end
+
+  def name_response(name) do
+    yes_or_no = IO.gets("Nice to meet you #{name}!  Want to play a guessing game?\n ")
+    yes_or_no = String.trim(yes_or_no)
+    maybe_play_game(yes_or_no)
+  end
+
+  # call level 3
+
+  def maybe_play_game("yes") do
+    answer = IO.gets("Wonderful!\n Give me 2 more numbers\n")
+
+    number_list =
+      String.split(answer, ",")
+      |> Enum.map(fn x -> String.trim(x) end)
+      |> Enum.map(fn x -> String.to_integer(x) end)
+
+    low = List.first(number_list)
+    high = List.last(number_list)
+    guess(low, high)
+  end
+
+  def maybe_play_game("no") do
+    IO.puts("Thanks for coming, goodbye!\n")
+  end
+
+  # call level 4
+
+  def guess(low, high) when low > high, do: guess(high, low)
 
   def guess(low, high) do
-    answer = IO.gets "Are you thinking of #{mid(low, high)}\n"
+    answer = IO.gets("Are you thinking of #{mid(low, high)}\n")
     answer = String.trim(answer)
     response(answer, low, high)
   end
 
-
-  #call level 2
-
+  # call level 5
 
   def mid(low, high) do
     div(low + high, 2)
@@ -22,23 +65,26 @@ defmodule GuessingGame do
   def response("bigger", low, high) do
     bigger(low, high)
   end
+
   def response("smaller", low, high) do
     smaller(low, high)
   end
+
   def response("yes", _low, _high) do
-    answer = IO.gets "I knew I could guess your number!  Care to play again?\n"
+    answer = IO.gets("I knew I could guess your number!  Care to play again?\n")
     answer = String.trim(answer)
-    maybe_play_again(answer)
+    maybe_play_game(answer)
   end
+
   def response(_answer, low, high) do
-    answer = IO.gets "That was not a correct response, try again\n"
+    answer =
+      IO.gets("That was not a correct response, use bigger, smaller, or yes and try again! :)\n")
+
     answer = String.trim(answer)
     response(answer, low, high)
   end
 
-
-  #call level 3
-
+  # call level 6
 
   def bigger(low, high) do
     new_low = min(high, mid(low, high) + 1)
@@ -48,18 +94,5 @@ defmodule GuessingGame do
   def smaller(low, high) do
     new_high = max(low, mid(low, high) - 1)
     guess(low, new_high)
-  end
-
-  def maybe_play_again("yes") do
-    answer = IO.gets "Wonderful!\n Give me 2 more numbers\n"
-    number_list = String.split(answer, ",")
-                  |> Enum.map(fn x -> String.trim(x) end)
-                  |> Enum.map(fn x -> String.to_integer(x) end)
-    low = List.first(number_list)
-    high = List.last(number_list)
-    guess(low, high)
-  end
-  def maybe_play_again("no") do
-    IO.puts("Thanks for playing, goodbye.\n")
   end
 end
--- test/guessing_game_test.exs
+++ test/guessing_game_test.exs
@@ -1,8 +1,4 @@
 defmodule GuessingGameTest do
   use ExUnit.Case
   doctest GuessingGame
-
-  test "greets the world" do
-    assert GuessingGame.hello() == :world
-  end
 end
